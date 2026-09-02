a0035b85507287c755f2a4e59c5315f0ef0785ce
CHAKRADHAR CHOWDARY GUNNAM <chakradhar.gunnam@gmail.com>
2026-09-02 15:29:30 +0530

    Time: 0 ms (100%), Space: 15 MB (12.47%) — Easy

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int,int> x;
+        unordered_map<int,int> x;
         for(int i=0;i<nums.size();i++){
             if(!x.empty() && x.contains(nums[i])) return {x[nums[i]],i};
             x[target-nums[i]] = i;
