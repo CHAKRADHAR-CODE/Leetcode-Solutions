406b0956b30189052093b0d793fe13c26962d23c
CHAKRADHAR CHOWDARY GUNNAM <chakradhar.gunnam@gmail.com>
2026-09-04 14:30:07 +0530

    Time: 0 ms (100%), Space: 32.4 MB (5.99%) — Easy

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        multiset<int> x(nums.begin(),nums.end());
+        int n = nums.size(),mx = INT_MIN;
+        for(int i=0;i<n;i++){
+            mx = max(mx,nums[i]);
+            if(mx-(*x.begin())<=k) return i;
+            x.erase(x.find(nums[i]));
+        }
+        return -1;
+    }
+};
